478ddf50331a2b64fdc93ed46b5a37cea36b25de
Divine Chidozie <igwedivine92@gmail.com>
2026-05-07 11:41:29 +0100

    updated changes

--- Portfolio-Website/script.js
+++ Portfolio-Website/script.js
@@ -8,6 +8,97 @@ const menu = document.getElementById("menu");
 const menuSpan = document.querySelectorAll(".menu-span");
 const mobileMenu = document.getElementById("mobileMenu");
 
+const liveDemoLink1 = document.getElementById("live-demo-link-1");
+const liveDemoLink2 = document.getElementById("live-demo-link-2");
+const githubLink1 = document.getElementById("github-link-1");
+const githubLink2 = document.getElementById("github-link-2");
+
+const liveLink = [liveDemoLink1, liveDemoLink2, githubLink1, githubLink2];
+liveLink.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    alert("We are working on this feature, stay with us.");
+  });
+});
+
+// Form Input
+const form = document.getElementById("form");
+
+const nameInput = document.getElementById("name-input");
+const emailInput = document.getElementById("email-input");
+const textAreaInput = document.querySelector("#textarea-input");
+
+// Form Error
+const nameError = document.getElementById("nameError");
+const emailError = document.getElementById("emailError");
+const textareaError = document.getElementById("textareaError");
+
+menu.addEventListener("click", function () {
+  mobileMenu.classList.toggle("active");
+});
+
+// const downloadResume = document.getElementById("download-resume");
+const viewResume = document.getElementById("view-resume");
+
+// downloadResume,
+const resumeButton = [viewResume];
+resumeButton.forEach((resume) => {
+  resume.addEventListener("click", (e) => {
+    e.preventDefault();
+    alert("Resume will be available for view soon, stay with us.");
+  });
+});
+
+const isValid = false;
+
+nameInput.textContent = "";
+emailInput.textContent = "";
+textAreaInput.textContent = "";
+
+const nameMessage = "Please enter your name";
+const emailMessage = "Please enter a valid email address";
+const teaxtAreaMessage = "Provide us with a Message";
+const secondTextAreaMessage = "Message must be more than 10 characters long";
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (nameInput.value.trim() === "") {
+    nameError.textContent = `${nameMessage}`;
+    return isValid;
+  }
+
+  if (!emailInput.value.trim()) {
+    emailError.textContent = emailMessage;
+    return isValid;
+  }
+
+  if (textAreaInput.value.trim() === "") {
+    textareaError.textContent = teaxtAreaMessage;
+  } else if (textAreaInput.value.trim().length <= 10) {
+    textareaError.textContent = secondTextAreaMessage;
+  }
+
+  if (!isValid) {
+  }
+});
+
+function clearNameError() {
+  nameError.textContent = "";
+}
+
+function clearEmailError() {
+  emailError.textContent = "";
+}
+
+const clearTextAreaError = () => {
+  textareaError.textContent = "";
+};
+
+nameInput.addEventListener("input", clearNameError);
+emailInput.addEventListener("input", clearEmailError);
+textAreaInput.addEventListener("input", clearTextAreaError);
+
 const headerMenuBackgroundColor = "oklch(49.6% 0.265 301.924)";
 const navLogoColor = "white";
 const headerMenuTransition = "0.3s ease-in";
@@ -32,39 +123,3 @@ function navHeaderScroll() {
   }
 }
 window.addEventListener("scroll", navHeaderScroll);
-
-menu.addEventListener("click", function () {
-  mobileMenu.classList.toggle("active");
-});
-
-const downloadResume = document.getElementById("download-resume");
-const viewResume = document.getElementById("view-resume");
-
-const resumeButton = [downloadResume, viewResume];
-resumeButton.forEach((resume) => {
-  resume.addEventListener("click", (e) => {
-    e.preventDefault();
-    alert("We are working hard to bring this feature live!");
-  });
-});
-
-// document.querySelectorAll(".live-demo").addEventListener("click", liveDemo);
-// document.querySelectorAll(".github").addEventListener("click", gitLink);
-
-// function demoLink() {
-//   liveDemo.forEach((link) => {
-//     link.addEventListener("click", (e) => {
-//       e.preventDefault();
-//       alert("We are still working on the project, stay turned!");
-//     });
-//   });
-// }
-
-// function gitLink() {
-//   github.forEach((link) => {
-//     link.addEventListener("click", (e) => {
-//       e.preventDefault();
-//       alert("We are still working on the project, stay turned!");
-//     });
-//   });
-// }
